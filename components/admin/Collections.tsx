3610f59e7e9f42d1b7d7dde590a1ca1befad7b33
sazypapi <zedekedochie@gmail.com>
2026-05-10 06:09:22 +0100

    Collections Dropdown Fixed

--- components/admin/Collections.tsx
+++ components/admin/Collections.tsx
@@ -12,87 +12,51 @@ function Collections({
 }) {
   const [collection, setCollection] = useState<string>("");
   return (
-    <>
-      <div className="w-full grid grid-cols-1 gap-4 sm:grid-cols-3 sm:gap-10 mt-2 sm:mt-3">
-        <div className="w-full hidden sm:inline-block">
-          <Label className="capitalize mb-1 sm:mb-2 text-xs sm:text-sm">
-            Collection
-          </Label>
-          <CreatableSelect
-            instanceId="seo-tags"
-            options={existingCollections.map((collection) => ({
-              value: collection,
-              label: collection,
-            }))}
-            onChange={(selected) =>
-              setCollection(selected ? selected.value : "")
-            }
-            placeholder="Select or create a collection."
-            styles={{
-              control: (baseStyles, state) => ({
-                ...baseStyles,
-                borderRadius: "7px",
-                border: "2px solid #d1d5db",
-                boxShadow: "0 1px 2px 0 rgba(209, 213, 219, 0.3)",
-                padding: "0px",
-              }),
-              placeholder: (baseStyles) => ({
-                ...baseStyles,
-                fontSize: "16px",
-              }),
-            }}
-          />
-
-          {/* Hidden input for form submission */}
-          <input type="hidden" name="collectionId" value={collection} />
-        </div>
-        <div className="w-full sm:hidden">
-          <Label className="capitalize mb-1 sm:mb-2 text-xs sm:text-sm">
-            Collection
-          </Label>
-          <CreatableSelect
-            instanceId="seo-tags-mobile"
-            options={existingCollections.map((collection) => ({
-              value: collection,
-              label: collection,
-            }))}
-            onChange={(selected) =>
-              setCollection(selected ? selected.value : "")
-            }
-            placeholder="Select or create a collection."
-            styles={{
-              control: (baseStyles, state) => ({
-                ...baseStyles,
-                borderRadius: "7px",
-                border: "2px solid #d1d5db",
-                boxShadow: "0 1px 2px 0 rgba(209, 213, 219, 0.3)",
-                padding: "0px",
-                minHeight: "36px",
-                height: "36px",
-              }),
-              placeholder: (baseStyles) => ({
-                ...baseStyles,
-                fontSize: "12px",
-              }),
-            }}
-          />
-
-          {/* Hidden input for form submission */}
-          <input type="hidden" name="collectionId" value={collection} />
-        </div>
-        <div>
-          <FormInput
-            name="material"
-            label="Material"
-            placeholder="material type"
-            type="text"
-          />
-        </div>
-        <div>
-          <StatusSelect />
-        </div>
+    <div className="w-full grid grid-cols-1 gap-4 sm:grid-cols-3 sm:gap-10 mt-2 sm:mt-3">
+      <div className="w-full">
+        <Label className="capitalize mb-1 sm:mb-2 text-xs sm:text-sm">
+          Collection
+        </Label>
+        <CreatableSelect
+          instanceId="collections"
+          options={existingCollections.map((collection) => ({
+            value: collection,
+            label: collection,
+          }))}
+          onChange={(selected) => setCollection(selected ? selected.value : "")}
+          placeholder="select or create a collection."
+          styles={{
+            control: (baseStyles) => ({
+              ...baseStyles,
+              borderRadius: "7px",
+              border: "2px solid #d1d5db",
+              boxShadow: "0 1px 2px 0 rgba(209, 213, 219, 0.3)",
+              padding: "0px",
+            }),
+            placeholder: (baseStyles) => ({
+              ...baseStyles,
+              fontSize: "16px",
+            }),
+            input: (baseStyles) => ({
+              ...baseStyles,
+              fontSize: "16px",
+            }),
+          }}
+        />
+        <input type="hidden" name="collectionId" value={collection} />
       </div>
-    </>
+      <div>
+        <FormInput
+          name="material"
+          label="Material"
+          placeholder="material type"
+          type="text"
+        />
+      </div>
+      <div>
+        <StatusSelect />
+      </div>
+    </div>
   );
 }
 
--- components/admin/SeoTags.tsx
+++ components/admin/SeoTags.tsx
@@ -18,69 +18,37 @@ function SeoTags({ product }: { product?: Product }) {
   return (
     <>
       <div className="w-full">
-        <div className="hidden sm:inline-block w-full">
-          {" "}
-          <Label className="capitalize mb-1 sm:mb-2 text-xs sm:text-sm">
-            SEO Tags
-          </Label>
-          <CreatableSelect
-            instanceId="seo-tags"
-            isMulti
-            options={predefinedTags.map((tag) => ({ value: tag, label: tag }))}
-            value={tags.map((tag) => ({ value: tag, label: tag }))}
-            placeholder="Select or create tags..."
-            styles={{
-              control: (baseStyles, state) => ({
-                ...baseStyles,
-                borderRadius: "7px",
-                border: "2px solid #d1d5db",
-                boxShadow: "0 1px 2px 0 rgba(209, 213, 219, 0.3)",
-                padding: "0px",
-                minHeight: "36px",
-                height: "36px",
-              }),
-              placeholder: (baseStyles) => ({
-                ...baseStyles,
-                fontSize: "16px",
-              }),
-            }}
-            onChange={(selected) => {
-              setTags(selected.map((option) => option.value));
-            }}
-            required
-          />
-        </div>
-        <div className="sm:hidden w-full">
-          <Label className="capitalize mb-1 sm:mb-2 text-xs sm:text-sm">
-            SEO Tags
-          </Label>
-          <CreatableSelect
-            instanceId="seo-tags-mobile"
-            isMulti
-            options={predefinedTags.map((tag) => ({ value: tag, label: tag }))}
-            value={tags.map((tag) => ({ value: tag, label: tag }))}
-            placeholder="select or create tags..."
-            styles={{
-              control: (baseStyles, state) => ({
-                ...baseStyles,
-                borderRadius: "7px",
-                border: "2px solid #d1d5db",
-                boxShadow: "0 1px 2px 0 rgba(209, 213, 219, 0.3)",
-                padding: "0px",
-              }),
-              placeholder: (baseStyles) => ({
-                ...baseStyles,
-                fontSize: "16px",
-              }),
-            }}
-            onChange={(selected) => {
-              setTags(selected.map((option) => option.value));
-            }}
-            required
-          />
-        </div>
-
-        {/* Hidden input for form submission */}
+        <Label className="capitalize mb-1 sm:mb-2 text-xs sm:text-sm">
+          SEO Tags
+        </Label>
+        <CreatableSelect
+          instanceId="seo-tags"
+          isMulti
+          options={predefinedTags.map((tag) => ({ value: tag, label: tag }))}
+          value={tags.map((tag) => ({ value: tag, label: tag }))}
+          placeholder="select or create tags..."
+          styles={{
+            control: (baseStyles) => ({
+              ...baseStyles,
+              borderRadius: "7px",
+              border: "2px solid #d1d5db",
+              boxShadow: "0 1px 2px 0 rgba(209, 213, 219, 0.3)",
+              padding: "0px",
+            }),
+            placeholder: (baseStyles) => ({
+              ...baseStyles,
+              fontSize: "16px",
+            }),
+            input: (baseStyles) => ({
+              ...baseStyles,
+              fontSize: "16px",
+            }),
+          }}
+          onChange={(selected) => {
+            setTags(selected.map((option) => option.value));
+          }}
+          required
+        />
         <input type="hidden" name="seoTags" value={JSON.stringify(tags)} />
       </div>
     </>
